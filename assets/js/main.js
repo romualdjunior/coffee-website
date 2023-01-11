e3ddfd9342423ec38abea996f0d68d78e84dfd66
romualdjunior <romuald.motchehokamguia@esprit.tn>
2023-01-11 17:50:27 +0100

    Adding Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,39 @@
 
 
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/*=============== MENU SHOW ===============*/
+// Variable if constant exist 
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        console.log("oui")
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+/*=============== MENU HIDDEN ===============*/
+// Variable if constant exist
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link")
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
@@ -13,13 +42,13 @@
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
 
-/* Default filter products */ 
+/* Default filter products */
 
 
-/* Link active products */ 
+/* Link active products */
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
